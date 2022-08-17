9d66d737f1d59bfe433877ebb9e28b278b135cb3
Augusto Araujo <augusto.aas@gmail.com>
2022-08-16 22:15:16 -0300

    Monta a arquitetura do projeto. Cria as classes dataContex, data conext factory, alunos, professor,alunoTurma, turma

---
+++ ApiDDD/ApiDDD.Domain/Contracts/Aluno.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiDDD.Domain.Contracts
+{
+    public class Aluno
+    {
+        #region Propriedades
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Matricula { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+        #endregion
+    }
+}
---
+++ ApiDDD/ApiDDD.Domain/Contracts/Professor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiDDD.Domain.Contracts
+{
+    public class Professor
+    {
+        #region Propriedades
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Cpf { get; set; }
+      
+        #endregion
+    }
+}
